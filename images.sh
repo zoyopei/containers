96ef60ede58939d4635eadf8a5db85031870a571
zoyopei <zoyopei@gmail.com>
2024-09-09 08:39:49 +0000

    update

--- images.sh
+++ images.sh
@@ -3,17 +3,22 @@
 IMAGE_LST=${IMAGE_LST-"images.txt"}
 REPO=$1
 
-while read image; do
-    if [[ $image =~ ^# ]]; then
+while read line; do
+    if [[ $line =~ ^# ]]; then
         continue
     fi
-    new_image=$(echo $image | awk -v REPO=$REPO -F"/" '{printf("%s/%s", REPO, $NF)}')
-    docker pull --platform linux/arm64 --platform linux/amd64 $image
-    docker buildx imagetools create \
-        --tag $new_image $image
+    read -r -a arr <<< "$line"
+    image=${arr[0]}
+    if [ ${#arr[@]} -eq 2 ]; then
+        new_image=${arr[1]}
+    else
+        new_image=$(echo $image | awk -v REPO=$REPO -F"/" '{printf("%s/%s", REPO, $NF)}')
+    fi
+    echo $image" -> "$new_image
+    #docker pull --platform linux/arm64 --platform linux/amd64 $image
+    docker buildx imagetools create --tag $new_image $image
     #docker tag $image $new_image
     #docker push $new_image
-    echo $image" -> "$new_image
 
 
 done < $IMAGE_LST
